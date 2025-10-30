7b1ad4570f346f7dc8ffb26d4ab422c1bbf1c5ea
Fadoua A <fades.devs@gmail.com>
2025-10-30 22:52:00 +0000

    changed user service

--- lab-w6-app/user-service/index.js
+++ lab-w6-app/user-service/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 const users = [
-{ id: 1, name: 'John Doe', email: 'john@example.com', age: 30 },
+{ id: 1, name: 'John Doe', email: 'john@example.com', age: 40 },
 { id: 2, name: 'Jane Smith', email: 'jane@example.com', age: 25 }
 ];
 
